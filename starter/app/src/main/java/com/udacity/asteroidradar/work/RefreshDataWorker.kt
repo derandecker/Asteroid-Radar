0b1a7b74de12a1fdf0bf114d217495a8bc3485fc
Deran Decker <me@derandecker.com>
2020-12-15 17:03:32 -1000

    Add RefreshDataWorker

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWorker.kt
@@ -0,0 +1,32 @@
+package com.udacity.asteroidradar.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.api.getEndDate
+import com.udacity.asteroidradar.api.getTodaysDate
+import com.udacity.asteroidradar.database.getDatabase
+import com.udacity.asteroidradar.repository.AsteroidsRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+        CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = AsteroidsRepository(database)
+
+        return try {
+            repository.refreshAsteroids(getTodaysDate(), getEndDate())
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+
+
+}
