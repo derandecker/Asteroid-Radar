cb7302e1350f77633c2d208fc3f349bfc99cf96e
Deran Decker <me@derandecker.com>
2020-12-15 22:19:06 -1000

    Change BindingAdapter name to make purpose clearer

--- starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -6,7 +6,7 @@ import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("statusIcon")
-fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
+fun bindAsteroidStatusIcon(imageView: ImageView, isHazardous: Boolean) {
     if (isHazardous) {
         imageView.setImageResource(R.drawable.ic_status_potentially_hazardous)
         imageView.contentDescription =
