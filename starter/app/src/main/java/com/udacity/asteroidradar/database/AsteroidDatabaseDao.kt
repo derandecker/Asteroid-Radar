fbc53eb1947d9f80ed4abadde5b34f06165fac8b
Deran Decker <me@derandecker.com>
2020-12-17 21:27:58 -1000

    Add query to retrieve DatabaseAsteroid based upon id

--- starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDatabaseDao.kt
@@ -11,6 +11,9 @@ interface AsteroidDatabaseDao {
     @Query("select * from databaseasteroid where closeApproachDate IN(:dates) order by closeApproachDate ")
     fun getAsteroids(dates: ArrayList<String>): LiveData<List<DatabaseAsteroid>>
 
+    @Query("select * from databaseasteroid where id = :id")
+    fun getSelectedAsteroid(id: Long): DatabaseAsteroid
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg asteroid: DatabaseAsteroid)
 }
